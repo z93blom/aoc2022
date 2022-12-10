33d28f60a82151b023f7ac9d3d0a3f45ca6c465d
Fredrik Blom <fredrik@familjenblom.se>
2022-12-10 07:08:47 +0100

    Day 10 solved

---
+++ Solutions/Y2022/Day10/Solution.cs
@@ -0,0 +1,94 @@
+using System.Text;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode.Y2022.Day10;
+
+class Solution : ISolver
+{
+    public int Year => 2022;
+    public int Day => 10;
+    public string GetName() => "Cathode-Ray Tube";
+
+    public IEnumerable<object> Solve(string input)
+    {
+        yield return PartOne(input);
+        yield return PartTwo(input);
+    }
+
+    static object PartOne(string input)
+    {
+        var registerValueDuringCycle = RegisterValueDuringCycle(input);
+
+        var signalStrength = new List<int>();
+        var cycleNumber = 20;
+        while (cycleNumber < registerValueDuringCycle.Count)
+        {
+            signalStrength.Add(registerValueDuringCycle[cycleNumber] * cycleNumber);
+            cycleNumber += 40;
+        }
+
+        var value = signalStrength.Take(6).Sum();
+        return value;
+    }
+
+    private static Dictionary<int, int> RegisterValueDuringCycle(string input)
+    {
+        var instructions = input.Lines().ToArray();
+        var cycle = 1;
+        var registerX = 1;
+        var registerValueDuringCycle = new Dictionary<int, int>(instructions.Length * 2);
+        foreach (var instruction in instructions)
+        {
+            var parts = instruction.Split(' ');
+            if (parts[0] == "noop")
+            {
+                registerValueDuringCycle.Add(cycle, registerX);
+                cycle++;
+            }
+            else if (parts[0] == "addx")
+            {
+                var v = int.Parse(parts[1]);
+                registerValueDuringCycle.Add(cycle, registerX);
+                cycle++;
+                registerValueDuringCycle.Add(cycle, registerX);
+                cycle++;
+                registerX += v;
+            }
+        }
+
+        registerValueDuringCycle.Add(cycle, registerX);
+        return registerValueDuringCycle;
+    }
+
+    static object PartTwo(string input)
+    {
+        var registerValueDuringCycle = RegisterValueDuringCycle(input);
+        var sb = new StringBuilder();
+        sb.AppendLine();
+        var cycle = 1;
+        while (cycle < registerValueDuringCycle.Count)
+        {
+            var spritePosition = registerValueDuringCycle[cycle];
+            var x = (cycle - 1) % 40;
+            if (spritePosition == x || spritePosition - 1 == x || spritePosition + 1 == x)
+            {
+                sb.Append('#');
+            }
+            else
+            {
+                sb.Append('.');
+            }
+
+            if (x == 39)
+            {
+                sb.AppendLine();
+            }
+
+            cycle++;
+        }
+
+        Console.Write(sb.ToString());
+
+        return "PHLHJGZA";
+    }
+}
